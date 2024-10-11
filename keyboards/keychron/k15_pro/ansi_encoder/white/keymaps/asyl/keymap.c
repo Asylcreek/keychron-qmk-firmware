2fbfd4eee077ce27835554b308338ad7daa7ecf6
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2024-10-12 00:51:03 +0100

    feat(keymap): Add SHORTCUTS layer

--- keyboards/keychron/k15_pro/ansi_encoder/white/keymaps/asyl/keymap.c
+++ keyboards/keychron/k15_pro/ansi_encoder/white/keymaps/asyl/keymap.c
@@ -21,20 +21,21 @@ enum layers {
     MAC_FN,
     WIN_BASE,
     WIN_FN,
+    SHORTCUTS,
 };
 
 enum custom_macros { LOG_OUT = SAFE_RANGE, N_FD_BK };
 
 // Left-hand home row mods
 #define HOME_A LSFT_T(KC_A)
-#define HOME_S LCTL_T(KC_S)
+#define HOME_Q LCTL_T(KC_Q)
 #define HOME_D LALT_T(KC_D)
 #define HOME_F LGUI_T(KC_F)
 
 // Right-hand home row mods
 #define HOME_J RGUI_T(KC_J)
 #define HOME_K LALT_T(KC_K)
-#define HOME_L RCTL_T(KC_L)
+#define HOME_P RCTL_T(KC_P)
 #define HOME_SCLN RSFT_T(KC_SCLN)
 
 // Macro Definitions
@@ -67,11 +68,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [MAC_BASE] = LAYOUT_90_ansi(
         KC_MUTE,    KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  BL_DOWN,   BL_UP,    KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  LOG_OUT,             KC_DEL,
         N_FD_BK,       KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
-        MC_2,       KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
-        MC_3,       KC_CAPS,  HOME_A,   HOME_S,   HOME_D,   HOME_F,   KC_G,      KC_H,     HOME_J,   HOME_K,   HOME_L,   HOME_SCLN,KC_QUOT,  KC_ENT,   KC_HOME,
+        MC_2,       KC_TAB,   HOME_Q,     KC_W,     KC_E,     KC_R,     KC_T,      KC_Y,     KC_U,     KC_I,     KC_O,     HOME_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
+        MC_3,       KC_CAPS,  HOME_A,   KC_S,   HOME_D,   HOME_F,   KC_G,      KC_H,     HOME_J,   HOME_K,   KC_L,   HOME_SCLN,KC_QUOT,  KC_ENT,   KC_HOME,
         MC_4,       _______,  KC_Z,     KC_X,   KC_C,   KC_V,    KC_B,     MO(MAC_FN),     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  _______,  KC_UP,
         /* MC_4,       KC_LSFT,            KC_Z,     KC_X,   KC_C,   KC_V,    KC_B,     MO(MAC_FN),     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT,  KC_UP, */
-        MC_5,       _______,  _______,  MO(MAC_FN), KC_SPC,  KC_ESC,           KC_SPC,     _______,        _______,            KC_LEFT,  KC_DOWN,  KC_RGHT),
+        MC_5,       _______,  _______,  MO(MAC_FN), LT(SHORTCUTS,KC_SPC),  KC_ESC,           KC_SPC,     _______,        _______,            KC_LEFT,  KC_DOWN,  KC_RGHT),
         /* MC_5,       KC_LCTL,  KC_LOPTN,           KC_LCMMD, KC_SPC,  KC_ESC,           KC_SPC,             KC_RCMMD,           KC_RCTL,            KC_LEFT,  KC_DOWN,  KC_RGHT), */
 
     [MAC_FN] = LAYOUT_90_ansi(
@@ -97,6 +98,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,    _______,  _______,  BL_DOWN,  _______,  _______,  _______,   _______,  _______,  _______,  _______,  _______,  _______,             _______,            _______,
         _______,    _______,            _______,  _______,  _______,  _______,   BAT_LVL,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,   _______,  _______,  _______,
         _______,    _______,  _______,            _______,  _______,  _______,             _______,            _______,            _______,             _______,  _______,  _______),
+
+    [SHORTCUTS] = LAYOUT_90_ansi(
+        _______,    _______,  _______,    _______,    _______,    _______,    _______,     _______,    _______,    _______,    _______,    _______,   _______,   _______,   _______,            _______,
+        _______,    _______,  _______,  _______,  _______,  _______,  _______,   _______,  LALT(KC_7),  LALT(KC_8),  LALT(KC_9),  LALT(KC_0),  _______,  _______,  _______,            _______,
+        _______,    LCTL(KC_TAB),  LGUI(KC_Q),  LGUI(KC_W),    _______,  LGUI(KC_R),  LGUI(KC_T),   _______,  LCTL(KC_U),  _______,  _______,  _______,  _______,  _______,  _______,            _______,
+        _______,    _______,  LGUI(KC_A),  _______,  LCTL(KC_D),  LGUI(KC_F),  _______,   _______,  _______,  _______,  _______,   _______,    _______,            _______,            _______,
+        _______,    _______,            _______,  LGUI(KC_X),  LGUI(KC_C),  LGUI(KC_V),   _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,    _______,  _______,            _______,  _______,  _______,             _______,            _______,            _______,            _______,  _______,  _______),
 };
 
 #if defined(ENCODER_MAP_ENABLE)
@@ -105,5 +114,6 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
     [MAC_FN]   = { ENCODER_CCW_CW(BL_DOWN, BL_STEP) },
     [WIN_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [WIN_FN]   = { ENCODER_CCW_CW(BL_DOWN, BL_UP) },
+    [SHORTCUTS]   = { ENCODER_CCW_CW(BL_DOWN, BL_UP) }, 
 };
 #endif // ENCODER_MAP_ENABLE
