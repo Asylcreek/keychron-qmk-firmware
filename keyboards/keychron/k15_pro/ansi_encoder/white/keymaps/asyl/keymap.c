357a237d44180803ac427062839fc1afc25caafb
Omokugbo Joseph Boro <omokugbobr@gmail.com>
2024-11-16 19:51:29 +0100

    refactor: Use combo for LCTL instead

--- keyboards/keychron/k15_pro/ansi_encoder/white/keymaps/asyl/keymap.c
+++ keyboards/keychron/k15_pro/ansi_encoder/white/keymaps/asyl/keymap.c
@@ -36,7 +36,7 @@ uint16_t COMBO_LEN = COMBO_LENGTH; // nifty trick continued
 const uint16_t PROGMEM TS_GUI[]     = {KC_T, KC_S, COMBO_END};
 const uint16_t PROGMEM RT_OPT[]     = {KC_R, KC_T, COMBO_END};
 const uint16_t PROGMEM RTS_SFT[]    = {KC_R, KC_T, KC_S, COMBO_END};
-const uint16_t PROGMEM ENTSPC_CTL[] = {LT(SHIFTED, KC_ENT), LT(SYM, KC_SPC), COMBO_END};
+const uint16_t PROGMEM ENTSPC_CTL[] = {LT(SHIFTED, KC_ENT), LT(SHORTS, KC_SPC), COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(TS_GUI, KC_LGUI),     //
@@ -47,14 +47,14 @@ combo_t key_combos[] = {
 
 // Left-hand home row mods
 /* #define HOME_A LSFT_T(KC_A) */
-#define HOME_B LCTL_T(KC_B)
+/* #define HOME_B LCTL_T(KC_B) */
 /* #define HOME_D LALT_T(KC_D) */
 /* #define HOME_F LGUI_T(KC_F) */
 
 // Right-hand home row mods
 /* #define HOME_J RGUI_T(KC_J) */
 /* #define HOME_K LALT_T(KC_K) */
-#define HOME_J RCTL_T(KC_J)
+/* #define HOME_J RCTL_T(KC_J) */
 /* #define HOME_SCLN RSFT_T(KC_SCLN) */
 
 // Macro Definitions
@@ -113,7 +113,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MC_2,       KC_TAB,         KC_B,            KC_L,     KC_D,                  KC_W,     KC_Z,      KC_QUOTE,  KC_F,     KC_O,     KC_U,     KC_J,     KC_SCLN,  KC_NO,   KC_NO,            KC_PGDN,
         MC_3,       TG(SHIFTED),   KC_N,              KC_R,     KC_T,                  KC_S,     KC_G,      KC_Y,      KC_H,     KC_A,     KC_E,     KC_I,     KC_COMM,  KC_NO,   KC_HOME,
         KC_LGUI,    KC_NO,  KC_Q,              KC_X,     KC_M,                  KC_C,     KC_V,      MO(MAC_FN),KC_K,     KC_P,     KC_DOT,   KC_MINS,  KC_SLSH,  KC_NO,  KC_UP,
-        MC_5,       HOMEROW,  HOMEROW_SCROLL,    LT(CMD,KC_ESC),   LT(SHIFTED,KC_ENT),  KC_LSFT,  LT(SYM,KC_SPC),    LT(SHORTS,KC_BSPC),    KC_NO,                                                 KC_LEFT,  KC_DOWN,  KC_RGHT),
+        MC_5,       HOMEROW,  HOMEROW_SCROLL,    LT(CMD,KC_ESC),   LT(SHIFTED,KC_ENT),  KC_LSFT,  LT(SHORTS,KC_SPC),    LT(SYM,KC_BSPC),    KC_NO,                                                 KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [MAC_FN] = LAYOUT_90_ansi(
         BL_TOGG,    _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,            _______,
